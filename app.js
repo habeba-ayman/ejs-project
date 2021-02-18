f551a493f93fe7c307ef33500d852e827313f508
habeba-ayman <habebaayman1994108@gmail.com>
2021-02-18 16:25:48 +0200

    add reports-table

--- app.js
+++ app.js
@@ -4,40 +4,46 @@ const app = express();
 app.set('view engine', 'ejs');
 
 //index page rout
-// app.get('/', function (req, res) {
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-//about page rout
-// app.get('/about', function (req, res) {
-//   res.sendFile(__dirname + '/about.html');
-// });
-
-//template
-// index page
 app.get('/', function (req, res) {
-  res.render('pages/index');
-});
-
-// about page
-app.get('/about', function (req, res) {
-  //res.render('pages/about');
-
-  var myInformation = [
+  // res.sendFile(__dirname + '/index.html');
+  var invoiceData = [
     {
-      name: 'Habeba',
-      organization: 'Concept Business Solutions',
-      birth_year: 1994,
+      id: 1,
+      customer: 'habeba',
+      customerEngagement: 'لا يوجد ارتباط',
+      createDate: '1-1-2020',
+      user: 'مستخدم 1',
+      notes: '',
+    },
+    {
+      id: 2,
+      customer: 'aziza',
+      customerEngagement: 'لا يوجد ارتباط',
+      createDate: '2-1-2020',
+      user: 'مستخدم 2',
+      notes: '',
+    },
+    {
+      id: 3,
+      customer: 'hind',
+      customerEngagement: 'لا يوجد ارتباط',
+      createDate: '3-1-2020',
+      user: 'مستخدم 3',
+      notes: '',
+    },
+    {
+      id: 4,
+      customer: 'mahmmud',
+      customerEngagement: 'لا يوجد ارتباط',
+      createDate: '4-1-2020',
+      user: 'مستخدم 4',
+      notes: '',
     },
   ];
-  var myJob = 'I am front end developer';
-
-  res.render('pages/about', {
-    myInformation: myInformation,
-    myJob: myJob,
+  res.render('pages/index', {
+    invoiceData: invoiceData,
   });
 });
-///
 
 app.listen(4000, function (req, res) {
   console.log('server is running');
